876bf3e9e598f1b94dd15bac6c64c03a3f42e34b
Patyi Árpád <PTY@budafok.lesaffre.hu>
2020-07-28 11:49:11 +0200

    Bootstrap new tags

--- webapp/index.js
+++ webapp/index.js
@@ -1,1 +1,6 @@
-sap.ui.define([], function() {'use strict'; alert("UI5 is ready"); });
+sap.ui.define([ ], 
+              function() {
+                  'use strict'; 
+                  alert("UI5 is ready for Árpád Patyi"); 
+                }
+            );
