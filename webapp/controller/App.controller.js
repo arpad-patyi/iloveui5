cc1dcbf9d5be78314a4f3af6c544b06c6d34ce48
Patyi Árpád <PTY@budafok.lesaffre.hu>
2020-07-30 13:23:43 +0200

    Step 15

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,29 +1,7 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], function(Controller, MessageToast, JSONModel, ResourceModel) {
+], function(Controller) {
   "use strict";
   return Controller.extend("development.controller.App", {
-    onInit: function() {
-      var oData = {
-        recipient: {
-          name: "World"
-        }
-      };
-      var oModel = new JSONModel(oData);
-      this.getView().setModel(oModel);
-      var i18nModel = new ResourceModel({
-        bundleName: "development.i18n.i18n"
-      });
-      this.getView().setModel(i18nModel, "i18n");
-    },
-    onShowHello: function() {
-      var oBundle = this.getView().getModel("i18n").getResourceBundle();
-      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-      MessageToast.show(sMsg);
-    }
   });
 });
