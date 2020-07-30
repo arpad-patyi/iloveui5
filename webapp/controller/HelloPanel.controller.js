b7b2d232fb1e254062907ae9df78e0d80b14c87b
Patyi Árpád <PTY@budafok.lesaffre.hu>
2020-07-30 13:59:05 +0200

    Step 16

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
   "use strict";
   return Controller.extend("development.controller.HelloPanel", {
      onShowHello : function () {
@@ -11,6 +12,21 @@ sap.ui.define([
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
         // show message
         MessageToast.show(sMsg);
+     },
+     onOpenDialog: function() {
+       var oView = this.getView();
+
+       if (!this.byId("helloDialog")) {
+        Fragment.load({
+          id: oView.getId(),
+          name: "development.view.HelloDialog"
+        }).then(function(oDialog) {
+          oView.addDependent(oDialog);
+          oDialog.open();
+        });
+       } else {
+         this.byId("helloDialog").open();
+       }
      }
   });
 });
